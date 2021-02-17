d86b5f55c7432c10f943ae654c2e1486587a4278
MarkoK <user.hawk2@air.com>
2021-02-17 12:31:54 +0100

    Replace ranger alias with a better one

--- shell_additions/aliases.sh
+++ shell_additions/aliases.sh
@@ -6,6 +6,6 @@ alias re-source='source ~/.bashrc'
 
 # Short commands for common tools.
 # With this alias, when returning from Ranger, current directory will be the last one you were positioned in while in Ranger.
-alias ra='ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"'
+alias ra='. ranger'
 # If you don-t want this behaviour, uncomment the alias bellow, and comment out the one above.
 # alias ra='ranger'
