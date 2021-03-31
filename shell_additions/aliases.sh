d1e04c50dde93b10ba20caf1d00c29aff9f8c487
MarkoK <user.hawk1@air.com>
2021-03-31 09:49:19 +0200

    Add alias for catkin build and pretty git logs

--- shell_additions/aliases.sh
+++ shell_additions/aliases.sh
@@ -3,6 +3,7 @@
 alias cd..='cd ..'
 alias cbt='catkin build --this'
 alias re-source='source ~/.bashrc'
+alias gitg='git log --graph --pretty=oneline --abbrev-commit --all'
 
 # Short commands for common tools.
 # With this alias, when returning from Ranger, current directory will be the last one you were positioned in while in Ranger.
